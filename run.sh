e5b4a43c387cf9ce490001d2593fe66fa0c890eb
Ravi Kumar Nimmi <rvkmr102@gmail.com>
2015-11-29 19:30:46 +0530

    installation instructions added

--- run.sh
+++ run.sh
@@ -1,2 +1,24 @@
+# copy .vimrc
+cp ~/.vimrc ~/.vimrc_backup
 cp ./.vimrc ~/.vimrc
+
+# install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# install nerdtree
+cd ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree.git
+
+# install powerline
+pip install --user powerline-status
+
+# install ctrlp
+cd ~/.vim
+git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
+
+# install silver search
+brew install the_silver_searcher
+cd ~/.vim/bundle && git clone https://github.com/rking/ag.vim 
+
 
