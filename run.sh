f7c1406688032737944e807fbd6ab7870e9b3101
Ravi Kumar Nimmi <rvkmr102@gmail.com>
2015-11-29 21:42:52 +0530

    cleanup

--- run.sh
+++ run.sh
@@ -1,42 +1,38 @@
 # copy .vimrc
-cp ~/.vimrc ~/.vimrc_backup
+mv ~/.vimrc ~/.vimrc_backup
 cp ./.vimrc ~/.vimrc
 
 # install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-# install nerdtree
+# go to bundle
 cd ~/.vim/bundle
+
+# install nerdtree
 git clone https://github.com/scrooloose/nerdtree.git
+
+# install ctrlp
+git clone https://github.com/kien/ctrlp.vim.git ctrlp.vim
+
+# install silver search
+brew install the_silver_searcher
+git clone https://github.com/rking/ag.vim 
+
+# solarized theme
+git clone git://github.com/altercation/vim-colors-solarized.git
+
+# fugitive
+git clone git://github.com/tpope/vim-fugitive.git
+
+# install meslo font
+git clone https://github.com/powerline/fonts.git
+open "fonts/Meslo/Meslo LG L Regular for Powerline.otf"
 
 # install pip
 sudo easy_install pip
 
 # install powerline
 pip install --user powerline-status
-
-# install ctrlp
-cd ~/.vim
-git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
-
-# install silver search
-brew install the_silver_searcher
-cd ~/.vim/bundle && git clone https://github.com/rking/ag.vim 
-
-# solarized theme
-cd ~/.vim/bundle
-git clone git://github.com/altercation/vim-colors-solarized.git
-mv vim-colors-solarized ~/.vim/bundle/
-
-# fugitive
-cd ~/.vim/bundle
-git clone git://github.com/tpope/vim-fugitive.git
-vim -u NONE -c "helptags vim-fugitive/doc" -c q
-
-# install meslo font
-cd ~
-git clone https://github.com/powerline/fonts.git
-open "fonts/Meslo/Meslo LG L Regular for Powerline.otf"
 
 
