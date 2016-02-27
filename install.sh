bfad846f0f7550028ce7cdffed752f3634b78e87
Ravi Kumar Nimmi <nimmi.ravikumar@gmail.com>
2016-02-28 01:17:39 +0530

    check before downloading packages

--- install.sh
+++ install.sh
@@ -1,3 +1,20 @@
+install_package(){
+    url=https://github.com/$1
+    project=$(basename "$url" ".${url##*.}")
+
+    if [ -d "$project" ]; then
+        echo "Updating" $project"..."
+        cd $project
+        git pull -q origin master
+        cd ..
+        echo $project "updated.\n"
+    else
+        echo "Installing" $project"..."
+        git clone -q $url
+        echo $project "installed\n"
+    fi
+}
+
 # copy .vimrc
 mv ~/.vimrc ~/.vimrc_backup
 cp ./.vimrc ~/.vimrc
@@ -10,35 +27,36 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 
 # install nerdtree
-git clone https://github.com/scrooloose/nerdtree.git
+install_package scrooloose/nerdtree.git
 
 # install ctrlp
-git clone https://github.com/ctrlpvim/ctrlp.vim.git ctrlp.vim
+install_package ctrlpvim/ctrlp.vim.git
 
 # install silver search
 brew install the_silver_searcher
-git clone https://github.com/rking/ag.vim 
+install_package rking/ag.vim.git
 
 # solarized theme
-git clone git://github.com/altercation/vim-colors-solarized.git
+install_package altercation/vim-colors-solarized.git
 
 # fugitive
-git clone git://github.com/tpope/vim-fugitive.git
+install_package tpope/vim-fugitive.git
 
 # nerdcommenter
-git clone https://github.com/scrooloose/nerdcommenter.git
+install_package scrooloose/nerdcommenter.git
 
 # install meslo font
-git clone https://github.com/powerline/fonts.git
+install_package powerline/fonts.git
 fonts/install.sh
 
 # install pip
 sudo easy_install pip
+sudo pip install --upgrade pip
 
 # install powerline
 pip install --user powerline-status
 
 brew install cmake
-git clone https://github.com/Valloric/YouCompleteMe.git
+install_package Valloric/YouCompleteMe.git
 cd YouCompleteMe
 ./install.py --clang-completer
